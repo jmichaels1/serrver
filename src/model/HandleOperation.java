d5c5f5e00759909fc91154bfad6b919e61171158
mjl1010 <manel@foxize.com>
2018-05-23 00:08:54 +0200

    Mejorado Másters.

--- src/model/HandleOperation.java
+++ src/model/HandleOperation.java
@@ -124,14 +124,72 @@ public class HandleOperation {
      */
     public void getMasters(Universidad universidad) throws IOException {
         ArrayList<Master> masters = new ArrayList<>();
-        for (int i = 0; i<30; i++) {
-            Master m = new Master(100+i*3, "MC"+i, "Máster en A"+i*33+"lborg");
-            if (i%6 == 1) {
-                m.setMasterVinculado(masters.get(i-1));
-                masters.get(i-1).setMasterVinculado(m);
-            }
-            masters.add(m);
-        }
+        int i = 100;
+
+        Master m1 = new Master(i++*3, "M-SAP-ERP", "Máster SAP en Sistemas de Información Integrados (ERP) - UPM");
+        Master m2 = new Master(i++*3, "M-NUT-BIO-ALI", "Máster en Nutrición y Biotecnología Alimentaria");
+        Master m3 = new Master(i++*3, "M-DIR-OPE-INN", "Máster en Dirección de Operaciones e Innovación");
+        Master m4 = new Master(i++*3, "M-ING-IND", "Máster en Ingeniería Industrial");
+        Master m5 = new Master(i++*3, "M-ACT-FIS-ENT-GES-DEP", "Máster en Actividad física: Entrenamiento y gestión deportiva");
+        Master m6 = new Master(i++*3, "M-ACT-FIS-SAL", "Máster en Actividad Física y Salud");
+        m5.setMasterVinculado(m6); m6.setMasterVinculado(m5);
+        Master m7 = new Master(i++*3, "M-GER-SOC", "Máster en Gerontología Social");
+        Master m8 = new Master(i++*3, "M-CIE-TEC-MAR", "Máster en Ciencia y Tecnología Marina");
+        Master m9 = new Master(i++*3, "M-DER-NEG-INT", "Máster en Derecho y Negocios Internacionales");
+        Master m10 = new Master(i++*3, "MA-DIR-FOT", "Máster anual de Dirección de fotografía");
+        Master m11 = new Master(i++*3, "MC-DIE-SEG-ALI-COL", "Máster en Dietética y seguridad alimentaria para colectividades (Certificado por la Cátedra Ferran Adrià)");
+        Master m12 = new Master(i++*3, "M-DIS-GES-DIR-PRO", "Máster en Diseño, Gestión y Dirección de Proyectos");
+        Master m13 = new Master(i++*3, "M-PSI-COM", "Máster en Psicología del Comportamiento");
+        Master m14 = new Master(i++*3, "M-EST-BIO-NAT-ACU", "Máster en Estudios Biologico Naturistas en Acupuntura");
+        Master m15 = new Master(i++*3, "M-GES-ADM-FIN", "Máster en Gestión Administrativa y Financiera");
+        Master m16 = new Master(i++*3, "M-GES-AUD-AMB", "Máster en Gestión y Auditorias Ambientales");
+        Master m17 = new Master(i++*3, "M-DIR-CIN", "Máster de Dirección de cine");
+        Master m18 = new Master(i++*3, "M-ADM-GES-EMP", "Máster en Administración y Gestión de Empresas");
+        Master m19 = new Master(i++*3, "M-DIR-CON-TUR", "Máster en Dirección y Consultoría Turística");
+        Master m20 = new Master(i++*3, "M-NEG-CHI-ASIPAC", "Máster en Negocios con China y Asia-Pacífico");
+        Master m21 = new Master(i++*3, "MCO-ADM-DBO", "Máster Administración de la Base de Datos Oracle v.12c - Oracle Certified Professional");
+        Master m22 = new Master(i++*3, "M-SEX-CLI-TER-PAR", "Máster en Sexología Clínica y Terapia de Parejas (ISEP Barcelona)");
+        Master m23 = new Master(i++*3, "M-DIR-MAR-GES-COM", "Master en Dirección de Marketing y Gestión Comercial");
+        Master m24 = new Master(i++*3, "M-SEC-INT-ALT-DIR", "Máster Secretariado Internacional de Alta Dirección (Barcelona)");
+        Master m25 = new Master(i++*3, "M-EXE-MBA", "Executive MBA");
+        Master m26 = new Master(i++*3, "M-DIR-GES-PRO-MOD", "Máster de Dirección y Gestión de Producto de Moda");
+        Master m27 = new Master(i++*3, "M-CIU-HER-CRO", "Máster en Cuidados de Heridas Crónicas");
+        Master m28 = new Master(i++*3, "ME-BUS-MAN", "Master in Business Management (English)");
+        Master m29 = new Master(i++*3, "MM-AUT-MAN-IND", "Doble máster en Automatización y Mantenimiento industrial");
+        Master m30 = new Master(i*3, "ME-INT-BUS-MAN", "Máster en International Business Management (English)");
+        m28.setMasterVinculado(m30); m30.setMasterVinculado(m28);
+
+        masters.add(m1);
+        masters.add(m2);
+        masters.add(m3);
+        masters.add(m4);
+        masters.add(m5);
+        masters.add(m6);
+        masters.add(m7);
+        masters.add(m8);
+        masters.add(m9);
+        masters.add(m10);
+        masters.add(m11);
+        masters.add(m12);
+        masters.add(m13);
+        masters.add(m14);
+        masters.add(m15);
+        masters.add(m16);
+        masters.add(m17);
+        masters.add(m18);
+        masters.add(m19);
+        masters.add(m20);
+        masters.add(m21);
+        masters.add(m22);
+        masters.add(m23);
+        masters.add(m24);
+        masters.add(m25);
+        masters.add(m26);
+        masters.add(m27);
+        masters.add(m28);
+        masters.add(m29);
+        masters.add(m30);
+
         message = new Dato("getMasters", masters);
         System.out.println("getMasters");
         dos.writeObject(message);
@@ -174,6 +232,16 @@ public class HandleOperation {
             DiaPlanificado p1 = new DiaPlanificado();
             DiaPlanificado p2 = new DiaPlanificado();
 
+            p1.setCalendarioBase(cb);
+            p1.setDia(cb.getDia());
+            p1.setMaster((Master) data.get("master1"));
+            p1.setUniversidad(cb.getUniversidad());
+
+            p2.setCalendarioBase(cb);
+            p2.setDia(cb.getDia());
+            p2.setMaster((Master) data.get("master2"));
+            p2.setUniversidad(cb.getUniversidad());
+
             if (finSemana.contains(i)) cb.setFestivo(true);
             else {
                 Sesion s = new Sesion();
@@ -194,11 +262,6 @@ public class HandleOperation {
 
                 if (i % 4 == 1) {
                     // es común
-                    p2.setCalendarioBase(cb);
-                    p2.setDia(cb.getDia());
-                    p2.setMaster((Master) data.get("master2"));
-                    p2.setUniversidad(cb.getUniversidad());
-
                     s.setMaster2(p2.getMaster());
                     s.setNota1("Hola");
                     s.setTipoAula("L");
@@ -209,8 +272,8 @@ public class HandleOperation {
                 p1.setSesion(s);
             }
 
-            if (p1.getDia() != 0) planificacionDias.add(p1);
-            if (p2.getDia() != 0) planificacionDias.add(p2);
+            planificacionDias.add(p1);
+            planificacionDias.add(p2);
         }
         System.out.println("getPlanificacionCalendarios");
         message = new Dato("getPlanificacionCalendarios", planificacionDias);
